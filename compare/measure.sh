65d57a247cf5c935c375de5cd76e22bb1e612442
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2019-12-22 19:57:56 +0100

    Updated measurement script

--- compare/measure.sh
+++ compare/measure.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 COMPILE_TIMES=4
 STARTUP_TIMES=4
-
+CLEAN=true
 
 function compileTime(){
-    pushd ../$1
     for (( i=0; i<COMPILE_TIMES; i++))
     do
+        pushd ../$1
         #make a clean first as we want to measure a full rebuild
-        eval $2
+	if ( $CLEAN )
+        then
+            if [ "$3" == "mvn" ]
+            then
+                ./mvnw clean
+            else
+                ./gradlew clean
+	    fi
+        fi
 
 	#Build the application and store the time needed to results
-        /usr/bin/time -o times -f "%e" $3
-	echo "Compile time $1:  $(cat times)" >> ../compare/results
+        startNS=$(date +"%s%N")
+        if [ "$3" == "mvn" ]
+        then
+            ./mvnw package
+        else
+            ./gradlew assemble
+        fi
+        popd
+        docker-compose build $2
+        endNS=$(date +"%s%N")
+        compiletime=$(echo "scale=2;($endNS-$startNS)/1000000000" | bc)
+	echo "Compile time $2: $compiletime" >> results
     done
-    popd
 }
 
 function startup(){
     for (( i=0; i<STARTUP_TIMES; i++))
     do
         #Rebuild the container to always have a startup from null
-        docker-compose stop $1
-        docker-compose rm -f $1
-        docker-compose build $1
+        docker-compose stop $2
+        docker-compose rm -f $2
+        docker-compose build $2
 
 	#Start the container and measure how long it takes untill we get a valid result
         startNS=$(date +"%s%N")
-        docker-compose up -d $1
+        docker-compose up -d $2
         curl http://localhost:8080/issue/550e8400-e29b-11d4-a716-446655440000/
         while [ $? -ne 0 ]; do
             sleep 0.3
@@ -35,13 +52,15 @@ function startup(){
         done
         endNS=$(date +"%s%N")
         startuptime=$(echo "scale=2;($endNS-$startNS)/1000000000" | bc)
-        echo "Startup with $1 took $startuptime" >> results
+        echo "Startup with $2 took $startuptime" >> results
+
         #Measure memory
-        docker stats --no-stream "compare_$2_1"  >> results
+	memory=$(docker stats --format "{{.MemUsage}}" --no-stream "compare_$2_1")
+        echo "Memory usage after startup $2 $memory" >> results
     done
 
     #Stop contains as they might influence ohter startup times
-    docker-compose stop $1
+    docker-compose stop $2
 }
 
 #Prepare
@@ -53,10 +72,12 @@ docker-compose up -d postgres
 sleep 3
 
 #measure individually
-compileTime "spring" "./mvnw clean" "./mvnw package"
-startup "spring"
-compileTime "micronaut" "./gradlew clean" "./gradlew assemble"
-startup "micronaut"
+compileTime "quarkus"   "quarkus"   "mvn"
+startup     "quarkus"   "quarkus"
+compileTime "spring"    "spring"    "mvn"
+startup     "spring"    "spring"
+compileTime "micronaut" "micronaut" "gradle"
+startup     "micronaut" "micronaut"
 
 #Output all results
 cat results;
